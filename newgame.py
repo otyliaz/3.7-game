bb9fbc419d72c4ba31409b8a99503efe8d24063b
otyliaz <otyliaz10@gmail.com>
2024-03-11 09:11:04 +1300

    initial commit

---
+++ newgame.py
@@ -0,0 +1,29 @@
+#store locations as objects
+#directions as attributes in the object
+#map does not need to be a grid
+
+class Map:    
+    def __init__(self, locations):
+        self.locations = locations
+        
+    def move():
+        pass
+
+list_locations = [{"name":"Town Hall",
+                   "desc": "the town hall",
+                   "dirs":{"north": "Crime Scene", "down":"Basement"},
+                   "items":""},
+                  {"name":"Crime Scene",
+                   "desc":"the crime scene",
+                   "dirs":{"south": "Town"},
+                   "items":"Knife"}
+                  ]
+
+class Location:
+    def __init__(self, name, desc, dirs):
+        pass
+
+class Player:
+    def move():
+        pass
+
