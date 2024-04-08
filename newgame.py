3e985638672b00e4d407cb77e03b456ab66e51fd
otyliaz <otyliaz10@gmail.com>
2024-04-09 09:06:35 +1200

    move button kinda works

--- newgame.py
+++ newgame.py
@@ -11,7 +11,11 @@ LOCATIONS_DICT = [{"name": "A",
                   {"name": "B",
                    "desc": "the crime scene",
                    "dests": ["A"],
-                   "items": ["Knife"]}
+                   "items": ["Knife"]},
+                  {"name": "C",
+                   "desc": "another place",
+                   "dests": ["B"],
+                   "items": []}
                   ]
 
 # what's the point of this
@@ -35,12 +39,7 @@ class Location:
 
     def __str__(self):
         """Returns a string containing the information for each location"""
-        return "You are at {}. \n{}. \nYou can go to {}.".format(str(self.name),
-                                                                 str(self.desc),
-                                                                 ', '.join(
-                                                                 str(dest) for dest in self.dests)
-                                                                 # joins destinations with a comma
-                                                                 )
+        return f"You are at {str(self.name)}. \n{str(self.desc)}. \nYou can go to {', '.join(str(dest) for dest in self.dests)}."
 
 
 locations = [Location(**location) for location in LOCATIONS_DICT]
@@ -72,14 +71,14 @@ class Player:
         """Changes the player's current_loc to the new location that they input."""
 
         # if the destination that the player inputs is valid,
-        if input_dest.upper() in self.current_loc.dests:
+        if input_dest in self.current_loc.dests:
             # get new location object using get_loc
             new_loc = get_loc(input_dest)
             self.current_loc = new_loc
-            print(player.current_loc)  # print description of location
+            return player.current_loc  # return description of location
 
         else:
-            print("That is not a valid move.")
+            return "That is not a valid move."
 
     def take(self, input_item):
         """If the input item matches an item in the location, 
@@ -92,10 +91,12 @@ class Player:
         """not take, but pick up and look closely"""
         if input_item in self.current.loc.items:
             pass
+        # TODO this
 
 
 player = Player([])
 
+# ------------Tkinter stuff---------------------
 
 # create tkinter window
 root = tk.Tk()
@@ -107,29 +108,50 @@ root.title('Game Window')
 root.geometry('600x400')
 root.resizable(False, False)
 
-# create text widget and specify size.
-text = Text(root, height=5, width=52)
-text.pack()
-
+# create text widget and specify size
+main_text = Text(root, height=10, width=52, fg="blue")
+main_text.pack()
 # insert text that i want to display
-text.insert('1.0', player.current_loc)
+main_text.insert('1.0', player.current_loc)
 # user can't edit the text
-text['state'] = 'disabled'
+main_text['state'] = 'disabled'
 
+button_frame = tk.Frame(root, highlightbackground="green",
+                        bg="blue", width=200, height=200)
+button_frame.pack()
+
+# ADD need to make a frame that has all the buttons in it so i can frame.clear() each time to clear the previous buttons
+# frame that has the text and the buttons. widget for text at top and frame for buttons under the text.
+# should use grid instead pack
+
+
+def move_button():
+    """"Makes separate buttons for each location. Runs after main move button is clicked"""
+
+    for dest in player.current_loc.dests:
+        go_button = ttk.Button(
+            button_frame,
+            text=f"move to {dest}",
+            # dest=dest sets the variable before the loop repeats
+            command=lambda dest=dest: [player.move(dest), update_loc_text()]
+        )
+
+        go_button.pack()
+
+
+def update_loc_text():
+    main_text.config(state='normal')  # enables the text widget
+    # inserts new text at end of old text with 2 new lines
+    main_text.insert('end', '\n\n' + str(player.current_loc))
+
+
+# button for main move
 move_button = ttk.Button(
-    root,
-    text='Move'
+    button_frame,
+    text='Move',
+    command=move_button  # command = creates new buttons for each direction
 )
-
 move_button.pack()
 
-# keeps the window displaying
+# keeps the tkinter window displaying
 root.mainloop()
-
-# print(player.current_loc)
-# player.move("B")
-# print(player.current_loc)
-# print(player.inv)
-# player.take("Knife")
-# print(player.inv)
-# print(player.current_loc.items)
